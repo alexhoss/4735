213b6f502b9f866d9162b3b86204122a889eae92
harpreetbrar1 <leopardbrar@gmail.com>
2019-11-26 15:55:57 -0800

    Added another test function thatchecks word Hello on browser

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,9 +1,35 @@
 var expect  = require('chai').expect;
 var request = require('request');
+var should = require('chai').should();
 
 it('Main page content', function(done) {
     request('http://localhost:3000' , function(error, response, body) {
+        //expect(body).to.equal('Hello Changeds world 4!');
         expect(body).to.equal('Hello Changeds world 4!');
+        should.exist(body)
         done();
+    });
+});
+
+it('Check for one word "Hello" on browser ', function(done) {
+    request('http://localhost:3000' , function(error, response, body) {
+        let word = 'Hello'
+        let string = body;
+        let arr = string.split(' ')
+        
+
+         for(let i = 0; i < arr.length; i++) {
+            if(word === arr[i]){
+                // console.log('Test Passed')
+                done();
+                return;
+                
+            }
+        }
+        //console.log('Test Failed')
+        should.not.exist('Hello')
+        done();
+        
+        
     });
 });
