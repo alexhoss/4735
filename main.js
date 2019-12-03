f713fa443d53f80b02017cc3acfc576d56ddc6de
alexhosseini <1xantom@gmail.com>
2019-12-03 14:18:29 -0800

    demo

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello my friends'))
+app.get('/', (req, res) => res.send('Hello world'))
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
