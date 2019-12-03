2c197689382cb91316a4923a80666c58058e3ea8
harpreetbrar1 <36082546+harpreetbrar1@users.noreply.github.com>
2019-12-03 14:27:59 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Hello world 2'))
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
